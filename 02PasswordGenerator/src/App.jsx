064b3dbeefe64857a186ee5c33830f8733b5e87c
sougata-jana <sougataj76@gmail.com>
2025-02-25 19:14:31 +0530

    file

--- 02PasswordGenerator/src/App.jsx
+++ 02PasswordGenerator/src/App.jsx
@@ -1,13 +1,28 @@
 
-import { useState, useCallback} from 'react'
+import { useState,useCallback } from 'react'
 import './App.css'
 
 function App() {
+  const [length,setLength] = useState(8)
+  const [NumberAllowed, setNUmberAllowed] = useState(false)
+  const [CharAllowed, setCharcterAllowed] = useState(false)
+  const [Password, setPassword] = useState("")
+  const PasswordGenerator = useCallback(() =>{
+    let pass = ""
+    let str = "ABCDEFFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrtstuvwxyz"
+    if(NumberAllowed) str += "0123456789"
+    if(CharAllowed) str += "!@#$%^&*`~[]{}"
+    for (let i = 1; i < array.length; i++){
+     let char = Math.floor(Math.random()* str.length+1)
+     pass = str.charAt(char)
+    }
 
-  
+    setPassword(pass)
+
+  }, [length,NumberAllowed,CharAllowed,setPassword])
   return (
     <>
-    <h1>i have Password</h1>
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text'></div>
     </>
   )
 }

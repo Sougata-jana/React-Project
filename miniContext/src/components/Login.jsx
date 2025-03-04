3030721d5792722c17ddb734d5b700969349107b
sougata-jana <sougataj76@gmail.com>
2025-03-04 16:55:51 +0530

    context

---
+++ miniContext/src/components/Login.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import UserContext from '../context/userContext'
+import { useState } from 'react'
+import { useContext } from 'react'
+
+
+
+function Login() {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const {setUser} = useContext(UserContext)
+    
+    const handlesubmit = (e) =>{
+        e.preventDefault()
+        setUser({username, password})
+    }
+  return (
+    <div>
+        <h2>Login</h2>
+        <input type="text" value={username} onChange={(e) =>setUsername(e.target.value)} placeholder="Username"/>
+        <input type="password" value={password} onChange={(e) =>setPassword(e.target.value)} placeholder="Password"/>
+        <button onClick={handlesubmit}>submit</button>
+    </div>
+  )
+}
+
+export default Login
---
+++ miniContext/src/components/Profile.jsx
@@ -0,0 +1,12 @@
+import React, {useContext} from 'react'
+import UserContext from '../context/userContext'
+
+function Profile() {
+
+    const {user} = useContext(UserContext)
+if(!user) return <h2> Please Login</h2>
+
+return <div>welcome {user.username}</div>
+}
+
+export default Profile
